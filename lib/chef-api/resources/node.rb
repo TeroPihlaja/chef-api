255a8859917744527368961b9ada4aaa71a420b0
Tero Pihlaja <tero.pihlaja@vilant.com>
2019-09-20 16:27:46 +0300

    Reverted (chef#56) because it breaks old functionality

--- lib/chef-api/resources/node.rb
+++ lib/chef-api/resources/node.rb
@@ -10,8 +10,6 @@ module ChefAPI
       attribute :normal,       type: Hash,   default: {}
       attribute :override,     type: Hash,   default: {}
       attribute :run_list,     type: Array,  default: []
-      attribute :policy_name,  type: String
-      attribute :policy_group, type: String
 
       # Enterprise Chef attributes
       attribute :chef_environment, type: String, default: '_default'
